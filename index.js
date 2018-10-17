55febb2d59957791bec9a26c57351b236450243e
Kangyun Wang <kevinwang8011@hotmail.com>
2018-10-17 20:56:23 +0200

    Looping two pieces and setting pan offset

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ console.log('Its gonna rain!');
 
 let audioContext = new AudioContext();
 
+const startLoop = (audioBuffer, panOffset = 0) => {
+	// Reads audiobuffer data and streams to other nodes
+	let sourceNode = audioContext.createBufferSource();
+	let pannerNode = audioContext.createStereoPanner();
+	sourceNode.buffer = audioBuffer;
+
+	sourceNode.loop = true;
+	// On arriving at loopStart (seconds) it will start looping forever
+	sourceNode.loopStart = 2.98;
+	sourceNode.loopEnd = 3.8;
+
+	pannerNode.pan.value = panOffset;
+
+	sourceNode.connect(pannerNode);
+	pannerNode.connect(audioContext.destination);
+
+	sourceNode.start(0, 2.98); // Start at the loop
+	// sourceNode.start();
+};
+
 fetch('./itsgonnarain.mp3')
 	// Get the response as binary ArrayBuffer Object
 	.then(res => res.arrayBuffer())
@@ -9,11 +29,7 @@ fetch('./itsgonnarain.mp3')
 	.then(arrBuffer => audioContext.decodeAudioData(arrBuffer))
 	.then(audioBuffer => {
 		console.log('Got audio: ', audioBuffer);
-		// Reads audiobuffer data and streams to other nodes
-		let sourceNode = audioContext.createBufferSource();
-		sourceNode.buffer = audioBuffer;
-		sourceNode.loop = true;
-		sourceNode.connect(audioContext.destination);
-		sourceNode.start();
+		startLoop(audioBuffer, 1);
+		startLoop(audioBuffer, -1);
 	})
 	.catch(e => console.error(e));
