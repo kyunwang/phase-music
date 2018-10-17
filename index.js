747a7e6e11d9f29b6235a221ebf0bdabfb5bb5c7
Kangyun Wang <kevinwang8011@hotmail.com>
2018-10-17 10:27:29 +0200

    Add gitignore and load and play audiofile

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('Its gonna rain!');
+
+let audioContext = new AudioContext();
+
+fetch('./itsgonnarain.mp3')
+	// Get the response as binary ArrayBuffer Object
+	.then(res => res.arrayBuffer())
+	// Decode into an playable form
+	.then(arrBuffer => audioContext.decodeAudioData(arrBuffer))
+	.then(audioBuffer => {
+		console.log('Got audio: ', audioBuffer);
+		let sourceNode = audioContext.createBufferSource();
+		sourceNode.buffer = audioBuffer;
+		sourceNode.connect(audioContext.destination);
+		sourceNode.start();
+	})
+	.catch(e => console.error(e));
